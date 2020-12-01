32a3a23a93bb961db0fd8d3b69a7028ea6862fd1
shouryasingroha2008 <72244139+shouryasingroha2008@users.noreply.github.com>
2020-12-01 20:43:41 +0530

    Delete activity2.js

--- activity2.js
+++
@@ -1,7 +0,0 @@
-function getScore() {
-    score=localStorage.getItem("Score");
-    document.getElementById("update").innerHTML="<h1>Score :"+score+"</h1>"
-}
-function back() {
-    window.location="activity_1.html";
-}
